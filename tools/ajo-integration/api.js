7e62bd1a7121904c57974241698bb44a4769c726
Damian Zehnder <zehnder@adobe.com>
2024-05-03 10:21:03 +0200

    fix: template authorization header

--- tools/ajo-integration/api.js
+++ tools/ajo-integration/api.js
@@ -1,15 +1,13 @@
-const templateHeaders = {
-  'Accept': '*/*',
-  'Content-Type': 'application/vnd.adobe.cjm.template.v1+json',
-  'Authorization': `Bearer ${sessionStorage.getItem('ims-token')} `,
-  'x-api-key': 'ajo-aem-integration',
-  'x-gw-ims-org-id': '908936ED5D35CC220A495CD4@AdobeOrg',
-  'x-sandbox-name': 'prod',
-};
-
 async function createTemplate(name, description, email) {
   return fetch('https://platform.adobe.io/journey/authoring/message/templates', {
-    headers: templateHeaders,
+    headers: {
+      'Accept': '*/*',
+      'Content-Type': 'application/vnd.adobe.cjm.template.v1+json',
+      'Authorization': `Bearer ${sessionStorage.getItem('ims-token')} `,
+      'x-api-key': 'ajo-aem-integration',
+      'x-gw-ims-org-id': '908936ED5D35CC220A495CD4@AdobeOrg',
+      'x-sandbox-name': 'prod',
+    },
     method: 'POST',
     body: JSON.stringify({
       "name": name || `Email ${Date.now()}`,
